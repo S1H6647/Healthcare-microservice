1cec59bea35275a32ae1e9bc8ee425a31fde51b2
Sanjit Hyanju <sanjithyanju99@gmail.com>
2026-04-01 13:54:49 +0545

    api-gateway

---
+++ api-gateway/src/main/java/com/healthcare/apigateway/filter/AuthFilter.java
@@ -0,0 +1,92 @@
+package com.healthcare.apigateway.filter;
+
+import com.healthcare.apigateway.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthFilter implements GlobalFilter, Ordered {
+
+    private static final List<String> PUBLIC_PATHS = List.of(
+            "/api/auth/register",
+            "/api/auth/login"
+    );
+    private final JwtService jwtService;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange,
+                             GatewayFilterChain chain) {
+
+        // let preflight requests pass through immediately
+        if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
+            return chain.filter(exchange);
+        }
+
+        String path = exchange.getRequest().getURI().getPath();
+        HttpMethod method = exchange.getRequest().getMethod();
+        log.info("AuthFilter intercepted request - path: {}, method: {}", path, method);
+
+        // skip auth for public paths
+        if (isPublicPath(path, method)) {
+            log.info("Skipping authentication for public path: {}", path);
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest()
+                .getHeaders()
+                .getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            log.warn("Missing or invalid Authorization header for path: {}", path);
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+
+        if (!jwtService.isTokenValid(token)) {
+            log.warn("Invalid JWT token for path: {}", path);
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String userId = jwtService.extractUserId(token);
+        String role = jwtService.extractRole(token);
+        String email = jwtService.extractEmail(token);
+
+        log.info("Injected Headers - userId: {}, role: {}, email: {}", userId, role, email);
+
+        ServerWebExchange mutatedExchange = exchange.mutate()
+                .request(exchange.getRequest().mutate()
+                        .header("X-User-Id", userId)
+                        .header("X-User-Role", role)
+                        .header("X-User-Email", email)
+                        .header(HttpHeaders.AUTHORIZATION, "")
+                        .build()
+                ).build();
+
+        return chain.filter(mutatedExchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1; // run before all the filters
+    }
+
+    private boolean isPublicPath(String path, HttpMethod method) {
+        return PUBLIC_PATHS.stream().anyMatch(path::startsWith);
+    }
+}
---
+++ api-gateway/src/main/java/com/healthcare/apigateway/service/JwtService.java
@@ -0,0 +1,55 @@
+package com.healthcare.apigateway.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+@Slf4j
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    public String extractUserId(String token) {
+        return String.valueOf(extractAllClaims(token).get("id"));
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            log.warn("Invalid JWT token: {}", token);
+            return false;
+        }
+    }
+
+
+}
